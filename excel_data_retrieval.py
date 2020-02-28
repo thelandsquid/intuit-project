694fd8d90ce2ac83df6bc38c8d37287cc5e66126
thelandsquid <bentheipadguy@gmail.com>
2020-02-28 12:35:51 -0600

    Added command line arguments to select gt and images paths

--- excel_data_retrieval.py
+++ excel_data_retrieval.py
@@ -4,5 +4,16 @@ def get_labels_from_excel(file_path,sheet_no,starting_row,row_amount):
     wb = xlrd.open_workbook(file_path)
     sheet = wb.sheet_by_index(sheet_no)
     for i in range(row_amount):
-        labels.append(sheet.row_slice(starting_row+i,0,sheet.row_len(starting_row+i)))
+        labels.append(remove_label_and_marks(str(sheet.row_slice(starting_row+i,0,sheet.row_len(starting_row+i))[1:])))
     return labels
+
+# Removed labels added by excel document and removed decimals from integers (some numbers with .0 at end are integers in the W-2)
+def remove_label_and_marks(str):
+    while str[0]!=':':
+        str=str[1:]
+    str=str[1:]
+    if str[0]=='\'' and str[-1]=='\'':
+        str = str[1:-1]
+    if str[-2:]=='.0':
+        str = str[:-2]
+    return str
--- tesseract_test.py
+++ tesseract_test.py
@@ -1,6 +1,8 @@
 import tesserocr
 import excel_data_retrieval as excel
 import time
+import argparse
+import os, fnmatch
 
 def image_accuracy_test(images, labels):
     correctForms, predictedForms = 0, 0
@@ -11,8 +13,9 @@ def image_accuracy_test(images, labels):
             api.SetImageFile(images[i])
             imageText = api.GetUTF8Text().lower()                   #converts the output of OCR to lowercase
             predictionAmount, correctPredictions = 0, 0
-            for j in range(1,len(labels[i])):
-                labelStringArray = remove_label_and_marks(str(labels[i][j])).split()
+            for j in range(len(labels[i])):
+                labelStringArray = (labels[i][j]).split('\\s')
+                print(labelStringArray)
                 for labelStr in labelStringArray:
                     if labelStr.lower() in imageText:                       #compare lowercase label with output of OCR
                         correctPredictions+=1
@@ -23,7 +26,6 @@ def image_accuracy_test(images, labels):
             predictedForms+=1
             if correctPredictions/predictionAmount>.999999:
                 correctForms+=1
-            # print(correctPredictions/predictionAmount)
             accuracyPercentages.append(correctPredictions/predictionAmount)
             times.append(int(round(time.time()*1000))-startTime)
 
@@ -31,17 +33,6 @@ def image_accuracy_test(images, labels):
     print('Forms Properly Recognized: ',correctForms)
     print('Average Processing Time: ',sum(times)/len(times),' milliseconds')
     print('Average Cell Accuracy: ',100*sum(accuracyPercentages)/len(accuracyPercentages),'% of cells correctly recognized\n')
-
-# Removed labels added by excel document and removed decimals from integers (some numbers with .0 at end are integers in the W-2)
-def remove_label_and_marks(str):
-    while str[0]!=':':
-        str=str[1:]
-    str=str[1:]
-    if str[0]=='\'' and str[-1]=='\'':
-        str = str[1:-1]
-    if str[-2:]=='.0':
-        str = str[:-2]
-    return str
 
 def test_fifty_fifty(clean_length, noisy_length):
     # Clean Data
@@ -60,20 +51,39 @@ def test_fifty_fifty(clean_length, noisy_length):
     labels = excel.get_labels_from_excel('fifty-fifty-testing\\labels.xlsx', 0, 1, 50)
     image_accuracy_test(images, labels)
 
-# dataSet is clean=0, noisy=1
-# imageNum corresponds to the code at the end of the image title
-def test_fifty_fifty_single_image(dataSet, imageNum):
-    switch = {
-        0: 'clean',
-        1: 'noisy',
-        2: 'intermediate'
-    }
-    image = ['fifty-fifty-testing\\W2_XL_input_'+switch[dataSet]+'_'+str(1000+imageNum)+'.jpg']
-    label = excel.get_labels_from_excel('fifty-fifty-testing\\labels.xlsx', dataSet, imageNum+1, 1)
-    image_accuracy_test(image,label)
+def test_realistic_noise():
+    # Realistic Noisy Data
+    print('REALISTIC NOISY DATA')
+    images = []
+    for i in range(100):
+        images.append('realistic-noise-testing\\W2_XL_input_noisy_' + str(1000 + i) + '.jpg')
+    labels = excel.get_labels_from_excel('realistic-noise-testing\\labels.xlsx', 0, 1, 100)
+    image_accuracy_test(images, labels)
 
 def main():
-    CLEAN, NOISY, INTERMEDIATE = 0, 1, 2
-    test_fifty_fifty_single_image(0, 0)
-    test_fifty_fifty(50,50)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--images_path', type=str, required=False,
+                        help='the path of the image set')
+    parser.add_argument('-gt', '--ground_truth_path', type=str, required=False,
+                        help='the path of the ground truth')
+    args = parser.parse_args()
+    if args.images_path is None:
+        test_realistic_noise()
+    elif args.ground_truth_path is None:
+        print('Must provide both ground_truth_path (-gt) and images_path (-i)')
+    else:
+        images, labels = [], []
+        onlyfiles = [f for f in os.listdir(args.images_path) if os.path.isfile(os.path.join(args.images_path, f))]
+        for file_name in onlyfiles:
+            if file_name.endswith(('.jpg','.jpeg','.gif','png')):
+                file_name_template = file_name.split('.',1)[0]+'.gt'
+                text_files = fnmatch.filter(os.listdir(args.ground_truth_path),file_name_template)
+                if(len(text_files)!=1):
+                    print('Improper amount of ground truth files for '+file_name)
+                    exit(1)
+                with open (os.path.join(args.ground_truth_path,text_files[0]),'r') as read_file:
+                    data = read_file.readlines()
+                    images.append(os.path.join(args.images_path,file_name))
+                    labels.append(data)
+        image_accuracy_test(images,labels)
 main()
